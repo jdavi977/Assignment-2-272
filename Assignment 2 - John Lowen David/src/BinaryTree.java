053aea16eef67b589464e3b4383dccb0a7d7ea89
Jawne3 <johnlowendavid@yahoo.ca>
2024-10-16 19:00:18 -0600

    Added junit test to treenode and binarytree classes

--- Assignment 2 - John Lowen David/src/BinaryTree.java
+++ Assignment 2 - John Lowen David/src/BinaryTree.java
@@ -1,12 +1,19 @@
+/**
+ * This class contains methods that allows the user to traverse through a binary tree.
+ */
 public class BinaryTree {
-    // Preorder Next
-    public static TreeNode preorderNext(TreeNode x) {
-        if (x.left != null) {
-            return x.left;
-        } else if (x.right != null) {
-            return x.right;
+	/**
+	 * Finds the next node through preorder traversal.
+	 * @param node is the current node
+	 * @return the next node through preorder traversal.
+	 */
+    public static TreeNode preorderNext(TreeNode node) {
+        if (node.left != null) {
+            return node.left;
+        } else if (node.right != null) {
+            return node.right;
         } else {
-            TreeNode current = x;
+            TreeNode current = node;
             while (current.parent != null) {
                 if (current == current.parent.left && current.parent.right != null) {
                     return current.parent.right;
@@ -14,17 +21,21 @@ public class BinaryTree {
                 current = current.parent;
             }
         }
-        return null; // No next node found
+        return null; 
     }
 
-    // Postorder Next
-    public static TreeNode postorderNext(TreeNode x) {
-        if (x.parent == null) {
-            return null; // x is the root, and postorder traversal ends at the root
+    /**
+     * Finds the next node through postorder traversal.
+     * @param node is the current node
+     * @return the next node through postorder traversal.
+     */
+    public static TreeNode postorderNext(TreeNode node) {
+        if (node.parent == null) {
+            return null; 
         }
 
-        TreeNode parent = x.parent;
-        if (parent.right == null || parent.right == x) {
+        TreeNode parent = node.parent;
+        if (parent.right == null || parent.right == node) {
             return parent;
         } else {
             TreeNode current = parent.right;
@@ -38,17 +49,21 @@ public class BinaryTree {
             return current;
         }
     }
-
-    // Inorder Next
-    public static TreeNode inorderNext(TreeNode x) {
-        if (x.right != null) {
-            TreeNode current = x.right;
+    
+    /**
+     * Finds the next node through inorder traversal.
+     * @param node is the current node
+     * @return the next node through inorder traversal.
+     */
+    public static TreeNode inorderNext(TreeNode node) {
+        if (node.right != null) {
+            TreeNode current = node.right;
             while (current.left != null) {
                 current = current.left;
             }
             return current;
         } else {
-            TreeNode current = x;
+            TreeNode current = node;
             while (current.parent != null && current == current.parent.right) {
                 current = current.parent;
             }
--- Assignment 2 - John Lowen David/src/TreeNode.java
+++ Assignment 2 - John Lowen David/src/TreeNode.java
@@ -1,10 +1,22 @@
-
+/**
+ * This class represents a node in the binary tree.
+ */
 public class TreeNode {
-    int val;
+	/**
+	 * The value of the node.
+	 */
+    int value;
+    /**
+     * References to the left, right, and parent nodes in the binary tree.
+     */
     TreeNode left, right, parent;
 
-    TreeNode(int val) {
-        this.val = val;
+    /**
+     * This is the constructor to create a TreeNode with a value.
+     * @param value is the value of the node.
+     */
+    TreeNode(int value) {
+        this.value = value;
         left = right = parent = null;
     }
 }
